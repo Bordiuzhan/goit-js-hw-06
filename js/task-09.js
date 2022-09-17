7aca074f169c6798c8f3f19ea3020b518a0fdc5c
Bordiuzhan <bordyujan@gmail.com>
2022-09-17 23:17:35 +0200

    task 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,15 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const bodyEl = document.querySelector('body');
+const btnChangeColorEl = document.querySelector('.change-color');
+const colorTextEl = document.querySelector('.color');
+
+btnChangeColorEl.addEventListener('click', onBtn);
+
+function onBtn() {
+  const color = getRandomHexColor();
+  bodyEl.style.backgroundColor = color;
+  colorTextEl.textContent = color;
+}
