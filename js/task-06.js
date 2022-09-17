58e61eb70544238d7680585a83687caddfb4da3e
Bordiuzhan <bordyujan@gmail.com>
2022-09-17 03:10:46 +0200

    task 6 ternarnik

--- js/task-06.js
+++ js/task-06.js
@@ -3,9 +3,7 @@ const inputEl = document.querySelector('#validation-input');
 inputEl.addEventListener('blur', onValidInput);
 
 function onValidInput() {
-  if (inputEl.value.trim().length < inputEl.dataset.length) {
-    inputEl.classList.add('invalid');
-  } else {
-    inputEl.classList.add('valid'), inputEl.classList.remove('invalid');
-  }
+  inputEl.value.trim().length < inputEl.dataset.length
+    ? (inputEl.classList = 'invalid')
+    : (inputEl.classList = 'valid');
 }
