b9c58a298c492ee43661b5503d08f5efc578d347
Bordiuzhan <bordyujan@gmail.com>
2022-09-17 21:49:47 +0200

    task 8.1 if

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,17 @@
+const formEl = document.querySelector('.login-form');
+console.dir(formEl);
 
+formEl.addEventListener('submit', onLoginBtn);
+
+function onLoginBtn(event) {
+  event.preventDefault();
+  const { email, password } = event.currentTarget.elements;
+  if (email.value.trim().length < 1 || password.value.trim().length < 1) {
+    alert('Всі поля повинні бути заповнені');
+  }
+  const dataForm = {
+    email: email.value,
+    password: password.value,
+  };
+  console.log(dataForm);
+}
