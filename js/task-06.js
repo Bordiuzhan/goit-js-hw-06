7eb0be1918c107265171215f6cfc616f0845c2dc
Bordiuzhan <bordyujan@gmail.com>
2022-09-18 16:52:52 +0200

    task 6 coreg

--- js/task-06.js
+++ js/task-06.js
@@ -3,7 +3,8 @@ const inputEl = document.querySelector('#validation-input');
 inputEl.addEventListener('blur', onValidInput);
 
 function onValidInput() {
-  inputEl.value.trim().length < inputEl.dataset.length
+  inputEl.value.trim().length < inputEl.dataset.length ||
+  inputEl.value.trim().length > inputEl.dataset.length
     ? (inputEl.classList = 'invalid')
     : (inputEl.classList = 'valid');
 }
