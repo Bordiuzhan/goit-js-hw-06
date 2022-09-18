2ee288890802f75cde58b0160955785d20c76fdb
Bordiuzhan <bordyujan@gmail.com>
2022-09-18 17:06:16 +0200

    task 8 coreg

--- js/task-08.js
+++ js/task-08.js
@@ -5,12 +5,15 @@ formEl.addEventListener('submit', onLoginBtn);
 function onLoginBtn(event) {
   event.preventDefault();
   const { email, password } = event.currentTarget.elements;
-  let dataForm = {};
 
-  email.value.trim().length < 1 || password.value.trim().length < 1
-    ? alert('Всі поля повинні бути заповнені')
-    : (dataForm = { email: email.value, password: password.value });
-
-  console.log(dataForm);
-  formEl.reset();
+  if (email.value.trim().length < 1 || password.value.trim().length < 1) {
+    alert('Всі поля повинні бути заповнені');
+  } else {
+    const dataForm = {
+      email: email.value,
+      password: password.value,
+    };
+    console.log(dataForm);
+    formEl.reset();
+  }
 }
